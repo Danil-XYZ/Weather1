673f8df50f89a6661ff2f6f2c11ca8ae0cb8808c
Danissimo <voindan@gmail.com>
2023-07-25 13:29:26 +0400

    Version 002

--- app/src/main/java/com/example/weather1/MainActivity.kt
+++ app/src/main/java/com/example/weather1/MainActivity.kt
@@ -3,41 +3,32 @@ package com.example.weather1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weather1.ui.mainScreen.MainViewScreen
+import com.example.weather1.ui.theme.Purple200
 import com.example.weather1.ui.theme.Weather1Theme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Weather1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainViewScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Weather1Theme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/weather1/ui/mainScreen/MainScreen.kt
@@ -0,0 +1,74 @@
+package com.example.weather1.ui.mainScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.weather1.R
+
+@Composable
+fun MainViewScreen() {
+    Scaffold() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier.fillMaxSize()
+
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Row() {
+
+                        Icon(
+                            modifier = Modifier
+                                .background(
+                                    color = Color.Transparent,
+                                    shape = RoundedCornerShape(100)
+                                )
+                                .clip(RoundedCornerShape(100))
+                                .clickable { },
+                            painter = painterResource(id = R.drawable.ic_baseline_add_24),
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+
+                    Text(
+                        text = "Mocow",
+                        modifier = Modifier.weight(1f),
+                        textAlign = TextAlign.Center
+                    )
+
+                        Icon(
+                            modifier = Modifier.clickable {  },
+                            painter = painterResource(id = R.drawable.ic_baseline_more_vert_24),
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "12314")
+                Text(text = "12314")
+                Text(text = "12314")
+            }
+        }
+    }
+}
